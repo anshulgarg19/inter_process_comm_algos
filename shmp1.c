208184d124b2da81d09b59cde52be3dc2eaf6892
anshulgarg19 <garg_anshul94@ymail.com>
2015-12-14 20:06:05 +0530

    Create shmp1.c

---
+++ shmp1.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include<sys/shm.h>
+#include<sys/ipc.h>
+
+#define SHM_SIZE 1024
+
+int main( void )
+{
+	key_t key;
+	key = ftok("shmp1.c",'a');
+	if( key == -1 )
+	{
+		perror("KEy ");
+		exit(1);
+	}
+	int shmid;
+	shmid = shmget( key, SHM_SIZE, IPC_CREAT | 0666 );
+	if( shmid == -1 )
+	{
+		perror("shmegt ");
+		exit(2);
+	}
+	char* data,*t;
+	data = shmat( shmid, (void*)0,0);
+	if( data == (void*)-1)
+	{
+		perror("shmat ");
+		exit(3);
+	}
+	char ch;
+	t = data;
+
+	*t = '*';
+	while( 1 )
+	{
+		ch = getchar();
+		*t = ch;
+		*++t = '*';
+	}
+	return 0;
+}
